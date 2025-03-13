8a2e8d4cca5c956cfa1b135e73f7c8420bc5eccc
Kerui Wu <99207376+keruiwu@users.noreply.github.com>
2025-03-13 14:28:11 -0400

    backend frame & login

---
+++ backend/service/login.js
@@ -0,0 +1,49 @@
+const jwt = require('jsonwebtoken');
+
+
+const authenticateToken = (req, res, next) => {
+    console.log('authenticate token');
+    const token =req.cookies.authtoken;
+    if (!token) {
+        console.log('\tno token');
+        res.json({"result": "ERROR", "message": "No token"});
+        return;
+    }
+    // verify token
+    try {
+        decoded = jwt.verify(token, process.env.MY_SECRET);
+        req.username = decoded.username;
+        req.title = decoded.title;
+        req.position = decoded.position;
+        console.log('\ttoken verified');
+        next();
+    } catch (err) {
+        console.log('\tinvalid token');
+        res.json({"result": "ERROR", "message": err.message});
+        return;
+    }
+};
+
+const authenticateAdmin = (req, res, next) => {
+    console.log('authenticate admin');
+    const token = req.cookies.authtoken;
+    if (!token) {
+        console.log('\tERR: no token');
+        res.json({"result": "ERROR", "message": "No token"});
+        return;
+    }
+    // verify token
+    try {
+        decoded = jwt.verify(token, process.env.MY_SECRET);
+        console.log('\ttoken verified');
+        req.username = decoded.username;
+        req.title = decoded.title;
+        req.position = decoded.position;
+        console.log('\tset username, title, position ', req.username, req.title, req.position);
+        next();
+    } catch (err) {
+        console.log('\tERR: invalid token');
+        res.json({"result": "ERROR", "message": err.message});
+        return;
+    }
+}

94fd8e8896fded36915caf373954fde094368208
Henry <119901185+hengruiren@users.noreply.github.com>
2025-03-20 15:23:15 -0400

    pos/title
    pos/title

---
+++ frontend/src/components/employee/position.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+function Position(props) {
+    const [position, setPosition] = useState(props.position);
+    const handleChange = (event) => {
+        setPosition(event.target.value);
+        props.onPositionChange(event.target.value);
+    };
+
+    useEffect(() => {
+        setPosition(props.position);
+    }, [props.position]);
+
+    return ( 
+        <Box sx={{ minWidth: props.minwidth }}>
+            <FormControl fullWidth>
+                <InputLabel>Branchs</InputLabel>
+                <Select
+                value={position}
+                label="position"
+                onChange={handleChange}
+                >
+                    <MenuItem value={'Troy'}>Troy</MenuItem>
+                    <MenuItem value={'SouthTroy'}>South Troy</MenuItem>
+                    <MenuItem value={'GreenIsland'}>Green Island</MenuItem>
+                    <MenuItem value={'Colonie'}>Colonie</MenuItem>
+                    <MenuItem value={'Latham'}>Latham</MenuItem>
+                    <MenuItem value={'Albany'}>Albany</MenuItem>
+       
+                </Select>
+            </FormControl>
+        </Box>
+     );
+}
+
+export default Position;
---
+++ frontend/src/components/employee/title.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+function Title(props) {
+    const [title, setTitle] = useState(props.title);
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+        props.onTitleChange(event.target.value);
+    }
+    useEffect(() => {
+        setTitle(props.title);
+    }, [props.title]);
+
+    return ( 
+        <Box sx={{ minWidth: props.minwidth }}>
+            <FormControl fullWidth>
+                <InputLabel>Title</InputLabel>
+                <Select
+                value={title}
+                label="title"
+                onChange={handleChange}
+                >
+                    <MenuItem value={'teller'}>Teller</MenuItem>
+                    <MenuItem value={'manager'}>Manager</MenuItem>
+                </Select>
+            </FormControl>
+        </Box>
+     );
+}
+
+export default Title;
+
+
+

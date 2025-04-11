9e0cbc916093087684a3ed22e47a889e81907229
Kerui Wu <99207376+keruiwu@users.noreply.github.com>
2025-04-10 22:15:36 -0400

    fix plus date error

--- frontend/src/components/employee/submitdate.jsx
+++ frontend/src/components/employee/submitdate.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { zhCN } from '@mui/x-date-pickers/locales';
 import 'dayjs/locale/zh-cn';
 
 import '../../App.css';
